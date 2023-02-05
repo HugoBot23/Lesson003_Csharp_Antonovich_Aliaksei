8d60da1e82dada723807f23e8104ae5fb12c8bd7
Aliaksei Antonovich <glados.lexa@gmail.com>
2023-02-05 10:32:43 +0300

    added task001

---
+++ task001/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+//Решение.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+if (number[0] == number[4] && number[1] == number[3])
+{
+  Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+  Console.Write($"Число {(number)} не является палиндромом");
+}
+
+//Решение.
+
+int[] array = new int[5];
+
+NumArray(array);
+NumbArray(array);
+
+void NumArray(int[] numbers)
+{
+  int mas = numbers.Length;
+  int index = 0;
+  while (index < mas)
+  {
+    Console.WriteLine("Введите каждую цыфру");
+    numbers[index] = int.Parse(Console.ReadLine());
+    index++;
+  }
+}
+
+void NumbArray(int[] numbers)
+{
+  int mas = numbers.Length;
+  if (numbers[0] == numbers[4] && numbers[1] == numbers[3])
+  {
+    Console.WriteLine($"Число {String.Join(",", numbers)} является палиндромом");
+  }
+  else
+  {
+    Console.WriteLine($"Число {String.Join(",", numbers)} не является палиндромом");
+  }
+}
+
