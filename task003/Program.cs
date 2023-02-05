30645f39a2dfefe9b199df31e3dedad9c7bce457
Aliaksei Antonovich <glados.lexa@gmail.com>
2023-02-05 10:33:47 +0300

    added taskoo3

---
+++ task003/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+//Решение.
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= number)
+{
+  Console.Write(Math.Pow(count, 3));
+  Console.Write(", ");
+  count++;
+}
+
+//Решение.
+
+Console.WriteLine("Втрое решение задачи");
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+int count1 = 1;
+
+while (count1 <= number1)
+{
+  int summa1 = count1 * count1 * count1;
+  Console.Write($"{summa1}, ");
+  count1++;
+}
